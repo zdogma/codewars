7dd0e44f4b2a187dc12ba360771d01a4f0416f63
z.dogma <z.dogma.asaki@gmail.com>
2021-02-18 17:41:28 +0900

    🆕 Add 55f2b110f61eb01779000053

---
+++ typescript/test/codewars/55f2b110f61eb01779000053/solution.ts
@@ -0,0 +1,23 @@
+import {assert} from 'chai';
+
+// Solution
+// Another Solution:
+//   return (Math.abs(a - b) + 1) * (a + b) / 2;
+//   ※ 等差数列の和として算出している
+function getSum(a: number, b: number): number {
+  const [from, to] = [a, b].sort((a, b) => a - b);
+  return Array.from(
+    {length: Math.abs(to - from + 1)},
+    (_, i) => from + i
+  ).reduce((a, v) => a + v, 0);
+}
+
+// Sample Test
+describe('getSum', () => {
+  it('Sample Tests', () => {
+    assert.strictEqual(getSum(0, -1), -1);
+    assert.strictEqual(getSum(0, 1), 1);
+    assert.strictEqual(getSum(1, 1), 1);
+    assert.strictEqual(getSum(-5, -1), -15);
+  });
+});
