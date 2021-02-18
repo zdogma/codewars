088372315b847b57708daa250706906e2fe26c7d
z.dogma <z.dogma.asaki@gmail.com>
2021-02-18 10:49:31 +0900

    🆕 Add 51f2d1cafc9c0f745c00037d

---
+++ typescript/test/codewars/51f2d1cafc9c0f745c00037d/solution.ts
@@ -0,0 +1,22 @@
+import {assert} from 'chai';
+
+// Solution
+// Another Solution:
+// 1) str.endsWith(ending);
+// 2) !ending.length || str.slice(-ending.length) === ending;
+const solution = (str: string, ending: string): boolean => {
+  const regexp = new RegExp(
+    ending.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&') + '$'
+  );
+  return str.match(regexp) !== null ? true : false;
+};
+
+// Sample Test
+describe('Sample Test Cases', () => {
+  it('Should return true or false', () => {
+    assert.equal(solution('abcde', 'cde'), true);
+    assert.equal(solution('abcde', 'abc'), false);
+    assert.equal(solution('abc', ''), true);
+    assert.equal(solution('aaa', ':-('), false);
+  });
+});
